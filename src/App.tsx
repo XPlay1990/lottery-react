475a39288581b20ca27e4fe81bfce85f0b05d04c
Jan Adamczyk <j_adamczyk@hotmail.com>
2021-11-24 23:51:42 +0100

    contract integration into react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FormEvent, useEffect, useState} from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+import {lotteryContract} from './crypto/LotteryContract'
+import {web3} from "./crypto/web3";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [contractOwner, setContractOwner] = useState("")
+    const [players, setPlayers] = useState([])
+    const [balance, setBalance] = useState("")
+    const [accounts, setAccounts] = useState([] as string[])
+    const [isWalletConnected, setIsWalletConnected] = useState(false)
+    const [etherValueToEnter, setEtherValueToEnter] = useState("0")
+    const [transactionState, setTransactionState] = useState("")
+
+
+    useEffect(() => {
+        async function getContractOwner() {
+            return await lotteryContract.methods.owner().call()
+        }
+
+        async function getPlayers() {
+            return await lotteryContract.methods.getPlayers().call()
+        }
+
+        async function getBalance() {
+            return await web3.eth.getBalance(lotteryContract.options.address)
+        }
+
+        getContractOwner().then(result => {
+            setContractOwner(result)
+        })
+        getPlayers().then(result => {
+            setPlayers(result)
+        })
+        getBalance().then(result => {
+            setBalance(web3.utils.fromWei(result, 'ether'))
+        })
+    }, [])
+
+    useEffect(() => {
+        async function getConnectedAccounts() {
+            return web3.eth.getAccounts()
+        }
+
+        getConnectedAccounts().then(connectedAccounts => {
+            if (connectedAccounts.length !== 0) {
+                setIsWalletConnected(true)
+            }
+            setAccounts(connectedAccounts)
+        })
+    }, [])
+
+
+    const ethEnabled = async () => {
+        if ((window as any).ethereum) {
+            await (window as any).ethereum.send('eth_requestAccounts');
+            // (window as any).web3 = new Web3((window as any).ethereum);
+            setIsWalletConnected(true)
+            setAccounts(await web3.eth.getAccounts())
+        }
+    }
+
+    const chooseWinner = async () => {
+        const accounts = await web3.eth.getAccounts()
+
+        await lotteryContract.methods.chooseWinner().send({
+            from: accounts[0]
+        })
+    }
+
+    async function enterLottery(event: FormEvent) {
+        event.preventDefault()
+
+        const accounts = await web3.eth.getAccounts()
+
+        setTransactionState("Waiting for Transaction to finish")
+        await lotteryContract.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei(etherValueToEnter, 'ether')
+        })
+        setTransactionState("Successfully entered the competition!")
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+            <h2>Lottery Contract</h2>
+            <p>
+                This Contract is managed by {contractOwner}, with currently {players.length} signed in that are
+                competing to win {balance} Ether
+            </p>
+            <hr/>
+            <form onSubmit={enterLottery}>
+                <h4>Want to enter?</h4>
+                <div>
+                    <label>Amount of ether to enter</label>
+                    <input value={etherValueToEnter} onChange={event => setEtherValueToEnter(event.target.value)}/>
+                </div>
+                <button>Enter</button>
+            </form>
+            {transactionState}
+            {
+                contractOwner === accounts[0] ?
+                    <Button variant="contained" onClick={chooseWinner}>Choose Winner</Button>
+                    : null
+            }
+            {
+                isWalletConnected ?
+                    null :
+                    <Button variant="contained" onClick={ethEnabled} disabled={!(window as any).ethereum}>Connect to
+                        Metamask</Button>
+            }
+        </div>
+    );
 }
 
 export default App;
---
+++ src/crypto/LotteryContract.ts
@@ -0,0 +1,72 @@
+import {web3} from './web3'
+import {AbiItem} from "web3-utils";
+
+export const address = '0x9c4ea125b0e5bcb8292ae6968a44cb451cd43295'
+export const abi: AbiItem[] = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "chooseWinner",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "enter",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getPlayers",
+        "outputs": [
+            {
+                "internalType": "address[]",
+                "name": "",
+                "type": "address[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "players",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+
+export const lotteryContract = new web3.eth.Contract(abi, address)
